42f760ce6d959e2057cd1c6db6b4dee484617c07
Ilia Barashkov <ibarashkov@list.ru>
2019-02-13 00:59:33 +0300

    retrive and create customer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="container d-flex flex-column">
+    <button type="button" class="btn btn-light mb-2 align-self-end" @click="showFrom=true">+ New</button>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>EMAIL</th>
+          <th>DESCRIPTION</th>
+          <th>DEFAULT SOURCE</th>
+          <th>CREATED</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(customer, index) in customers" :key="index">
+          <td>{{customer.email}}</td>
+          <td>{{customer.description}}</td>
+          <td>{{customer.default_source}}</td>
+          <td>{{customer.created | convertTimestamp}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <form v-if="showFrom" class="align-self-center w-25 border rounded p-3" action>
+      <h5>Create new customer</h5>
+      <div class="form-group">
+        <label class="label" for="email">Email</label>
+        <input
+          v-model="newCustomer.email"
+          id="email"
+          name="email"
+          type="email"
+          class="form-control w-100"
+        >
+      </div>
+      <div class="form-group">
+        <label class="label" for="description">Description</label>
+        <input
+          v-model="newCustomer.description"
+          id="description"
+          name="description"
+          type="text"
+          class="form-control w-100"
+        >
+      </div>
+      <div class="row d-flex justify-content-between p-3">
+        <button @click="showFrom=false" type="button" class="btn btn-secondary">Cancel</button>
+        <button @click="createCustomer" type="button" class="btn btn-primary">Create Customer</button>
+      </div>
+    </form>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      apiKey: "pk_test_2DBellUav75OL6TdEF8AoRH6",
+      secretKey: "sk_test_p4wW7fvIYIgPZBdj6gdYsdGS",
+      customers: [],
+      showFrom: false,
+      newCustomer: {
+        email: "",
+        description: ""
+      }
+    };
+  },
+  methods: {
+    getAuthHeader() {
+      return { Authorization: `Bearer ${this.secretKey}` };
+    },
+    createCustomer() {
+      this.showFrom = false;
+
+      axios
+        .post(
+          "https://api.stripe.com/v1/customers",
+          `email=${this.newCustomer.email};description=${
+            this.newCustomer.description
+          }`,
+          {
+            headers: {
+              Authorization: `Bearer ${this.secretKey}`,
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+          }
+        )
+        .then(res => {
+          this.retriveCustomers();
+          this.newCustomer.email = "";
+          this.newCustomer.description = "";
+        });
+    },
+    retriveCustomers() {
+      axios
+        .get("https://api.stripe.com/v1/customers", {
+          headers: this.getAuthHeader()
+        })
+        .then(res => {
+          this.customers = res.data.data;
+        });
+    }
+  },
+  filters: {
+    convertTimestamp(value) {
+      return new Date(value * 1000).toUTCString();
+    }
+  },
+  beforeMount() {
+    this.retriveCustomers();
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
